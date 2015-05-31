c6a2701f645eacd13fad12ba06776a19a8a6343d
Joe Harvell <jharvell@dogpad.tk>
2015-05-31 17:54:22 -0500

    Fix debugging in pidff_init_fields

--- drivers/hid/usbhid/hid-pidff.c
+++ drivers/hid/usbhid/hid-pidff.c
@@ -1082,14 +1082,14 @@ static int pidff_init_fields(struct pidff_device *pidff, struct input_dev *dev)
 	int envelope_ok = 0;
 	int error = 0;
 
-	hid_dbg("Searching for SetEffect fields");
+	hid_dbg(pidff->hid,"Searching for SetEffect fields");
 	if (PIDFF_FIND_FIELDS(set_effect, PID_SET_EFFECT, 1)) {
 		hid_err(pidff->hid, "unknown set_effect report layout\n");
 //		return -ENODEV;
 		error = -ENODEV;
 	}
 
-	hid_dbg("Searching for BlockLoad fields");
+	hid_dbg(pidff->hid,"Searching for BlockLoad fields");
 	PIDFF_FIND_FIELDS(block_load, PID_BLOCK_LOAD, 0);
 	if (!pidff->block_load[PID_EFFECT_BLOCK_INDEX].value) {
 		hid_err(pidff->hid, "unknown pid_block_load report layout\n");
@@ -1097,21 +1097,21 @@ static int pidff_init_fields(struct pidff_device *pidff, struct input_dev *dev)
 		error = -ENODEV;
 	}
 
-	hid_dbg("Searching for EffectOperation fields");
+	hid_dbg(pidff->hid,"Searching for EffectOperation fields");
 	if (PIDFF_FIND_FIELDS(effect_operation, PID_EFFECT_OPERATION, 1)) {
 		hid_err(pidff->hid, "unknown effect_operation report layout\n");
 //		return -ENODEV;
 		error = -ENODEV;
 	}
 
-	hid_dbg("Searching for BlockFree fields");
+	hid_dbg(pidff->hid,"Searching for BlockFree fields");
 	if (PIDFF_FIND_FIELDS(block_free, PID_BLOCK_FREE, 1)) {
 		hid_err(pidff->hid, "unknown pid_block_free report layout\n");
 //		return -ENODEV;
 		error = -ENODEV;
 	}
 
-	hid_dbg("Searching for SetEnvelope fields");
+	hid_dbg(pidff->hid,"Searching for SetEnvelope fields");
 	if (!PIDFF_FIND_FIELDS(set_envelope, PID_SET_ENVELOPE, 1))
 		envelope_ok = 1;
 
@@ -1136,7 +1136,7 @@ static int pidff_init_fields(struct pidff_device *pidff, struct input_dev *dev)
 
 	if (test_bit(FF_CONSTANT, dev->ffbit))
 	{
-	    hid_dbg("Searching for SetConstant fields");
+	    hid_dbg(pidff->hid,"Searching for SetConstant fields");
 	    if(PIDFF_FIND_FIELDS(set_constant, PID_SET_CONSTANT, 1)) {
 	        hid_warn(pidff->hid, "unknown constant effect layout\n");
 	        clear_bit(FF_CONSTANT, dev->ffbit);
@@ -1144,7 +1144,7 @@ static int pidff_init_fields(struct pidff_device *pidff, struct input_dev *dev)
 	}
 
 	if (test_bit(FF_RAMP, dev->ffbit)) {
-	    hid_dbg("Searching for SetRamp fields");
+	    hid_dbg(pidff->hid,"Searching for SetRamp fields");
 	    if(PIDFF_FIND_FIELDS(set_ramp, PID_SET_RAMP, 1)) {
 	        hid_warn(pidff->hid, "unknown ramp effect layout\n");
 	        clear_bit(FF_RAMP, dev->ffbit);
@@ -1155,7 +1155,7 @@ static int pidff_init_fields(struct pidff_device *pidff, struct input_dev *dev)
 	     test_bit(FF_DAMPER, dev->ffbit) ||
 	     test_bit(FF_FRICTION, dev->ffbit) ||
 	     test_bit(FF_INERTIA, dev->ffbit))) {
-	    hid_dbg("Searching for SetCondition fields");
+	    hid_dbg(pidff->hid,"Searching for SetCondition fields");
 	    if(PIDFF_FIND_FIELDS(set_condition, PID_SET_CONDITION, 1)) {
 	        hid_warn(pidff->hid, "unknown condition effect layout\n");
 	        clear_bit(FF_SPRING, dev->ffbit);
@@ -1166,7 +1166,7 @@ static int pidff_init_fields(struct pidff_device *pidff, struct input_dev *dev)
 	}
 
 	if (test_bit(FF_PERIODIC, dev->ffbit)) {
-	    hid_dbg("Searching for SetPeriodic fields");
+	    hid_dbg(pidff->hid,"Searching for SetPeriodic fields");
 	    if(PIDFF_FIND_FIELDS(set_periodic, PID_SET_PERIODIC, 1)) {
 	        hid_warn(pidff->hid, "unknown periodic effect layout\n");
 	        clear_bit(FF_PERIODIC, dev->ffbit);
@@ -1174,10 +1174,10 @@ static int pidff_init_fields(struct pidff_device *pidff, struct input_dev *dev)
 	}
 
 
-	hid_dbg("Searching for Pool fields");
+	hid_dbg(pidff->hid,"Searching for Pool fields");
 	PIDFF_FIND_FIELDS(pool, PID_POOL, 0);
 
-	hid_dbg("Searching for DeviceGain fields");
+	hid_dbg(pidff->hid,"Searching for DeviceGain fields");
 	if (!PIDFF_FIND_FIELDS(device_gain, PID_DEVICE_GAIN, 1))
 		set_bit(FF_GAIN, dev->ffbit);
 
